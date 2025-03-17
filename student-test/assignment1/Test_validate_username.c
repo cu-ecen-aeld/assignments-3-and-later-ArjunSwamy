5dbc4b08aa04343f228342c3118bb770e26a209c
ArjunSwamy <jeykar99@gmail.com>
2025-03-17 12:39:07 -0500

    unit_test good

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "../../examples/autotest-validate/autotest-validate.h"
 #include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
+#include "stdio.h"
 
 /**
 * This function should:
@@ -18,5 +19,6 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+   // TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+  TEST_ASSERT_EQUAL_STRING(my_username(),malloc_username_from_conf_file());
 }
