9236f903d2aeeabe76575e95041d6f38f76fa092
ArjunSwamy <jeykar99@gmail.com>
2025-08-18 14:41:10 -0500

    functions need testing

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+    if(cmd == NULL){
+        return -1;
+    }
+    return system(cmd);
 }
 
 /**
@@ -58,10 +60,23 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    pid_t pid;
+    int ret = 0;;
+    pid = fork();
+    if(pid<0){
+        return -1;
+    }
+    if(pid==0){
+        //in child
+        ret = execv(command[0],*(command[1]));
+    }
+    else{
+        wait(pid);
+    }
 
     va_end(args);
 
-    return true;
+    return ret;
 }
 
 /**
@@ -92,8 +107,27 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int ret = 0;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0)
+        return -1;
+    pid_t pid;
+    switch(pid = fork()){
+        case -1: 
+            return -1;
+        case 0: 
+            if (dup2(fd, 1) < 0){
+                 return -1;
+                }
+            close(fd);
+            ret = execv(command[0],*(command[1]));
+        default:
+            close(fd);
+            wait(pid);
+
+    }
 
     va_end(args);
 
-    return true;
+    return ret;
 }
